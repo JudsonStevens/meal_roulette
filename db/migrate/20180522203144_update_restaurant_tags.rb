fa38f610b4e37bee9fbb14637eb1277c6818cc54
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-22 16:51:32 -0600

    Working on restaurant seed data

---
+++ db/migrate/20180522203144_update_restaurant_tags.rb
@@ -0,0 +1,5 @@
+class UpdateRestaurantTags < ActiveRecord::Migration[5.2]
+  def change
+    add_column :restaurant_tags, :id, :primary_key
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+r1 = Restaurant.create(name: "Ophelia's Electric Soapbox", phone_number: 3039938023, address: '1215 20th Street, Denver CO', yelp_review_link: 'https://www.yelp.com/biz/ophelias-electric-soapbox-denver')
+Tag.create(type: 'Greek')
+Tag.create(type: 'French')
+Tag.create(type: 'American')
+Tag.create(type: 'Asian')
+Tag.create(type: 'Venezuelan')
+Tag.create(type: 'Craft Beer')
