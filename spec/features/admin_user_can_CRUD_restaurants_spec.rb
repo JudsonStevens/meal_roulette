6beee2b44b0da5352ca2697d32b74866532652a1
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-22 14:15:27 -0600

    Final implementation of admin restaurant creation

--- spec/features/admin_user_can_CRUD_restaurants_spec.rb
+++ spec/features/admin_user_can_CRUD_restaurants_spec.rb
@@ -19,5 +19,29 @@ describe 'Admin user' do
       expect(page).to have_content(@restaurant1.address)
       expect(page).to have_content(@restaurant1.phone_number)
     end
+
+    it 'can create a new restaurant after clicking create new restaurant' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+      name = 'Bobbys Fire'
+      address = 'Newcastle'
+      phone_number = 3342323333
+      yelp_review_link = 'yelp.com'
+      visit(admin_restaurants_path)
+
+      click_on('New Restaurant')
+
+      fill_in(:restaurant_name, with: name)
+      fill_in(:restaurant_address, with: address)
+      fill_in(:restaurant_phone_number, with: phone_number)
+      fill_in(:restaurant_yelp_review_link, with: yelp_review_link)
+
+      click_on('Create Restaurant')
+
+      expect(page).to have_content(name)
+      expect(page).to have_content(address)
+      expect(page).to have_content(phone_number)
+      expect(page).to have_content(yelp_review_link)
+    end
+      
   end
 end
