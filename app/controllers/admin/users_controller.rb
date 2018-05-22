a81bfe507069aa21ed4952cce892ba3d91f72f3d
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-22 08:11:31 -0600

    Admin user creation finished

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,27 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to admin_users_path, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render '/admin/users/new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :password)
+  end
 end
