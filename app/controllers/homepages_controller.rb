e2e6455b15dd5ca81c291714ec3a55f99eeac6e1
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-23 19:53:16 -0600

    Working on generating the list of restaurants for the wheel

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -5,7 +5,12 @@ class HomepagesController < ApplicationController
 
   def show
     if current_user
-
+      user = User.find(current_user)
+      user.tag_users.where('preference < 1').map { |tag_user| tag_user.tag.type }
+        
+      
+    else
+      @new_array = Restaurant.all.take(10).map { |restaurant| restaurant.name }
     end
   end
 end
